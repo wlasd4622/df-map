e81bd1b592dbed62b75a6f0e23d435ee99c123e7
王文龙 <357934522@qq.com>
2019-08-10 22:19:50 +0800

    .

--- server/main.js
+++ server/main.js
@@ -1,13 +1,25 @@
 let axios = require('axios');
 let list = [];
 let fs = require('fs')
+let ak = `WSf2NwqAoLrzbQiU3ZFzxtVWn1OQxQiH`;
 async function main() {
+    console.log(`>>>main`);
     await getList();
     console.log(list);
+    for (let i = 0; i < list.length; i++) {
+        try {
+            let location = await getDetail(list[i].activity_id);
+            list[i].location = location;
+        } catch (err) {
+            console.log(err);
+        }
+    }
     fs.writeFileSync('./web/src/assets/js/list.js', `module.exports = ${JSON.stringify(list)}`);
+    console.log(`END`);
 }
 
 function sleep(ms = 300) {
+    console.log(`>>>sleep`);
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve()
@@ -15,10 +27,29 @@ function sleep(ms = 300) {
     })
 }
 
-
+async function getDetail(activityId) {
+    console.log();
+    let url = `https://open.mishifeng.com/open/activity/detail?activity_id=${activityId}`
+    return new Promise((resolve, reject) => {
+        axios.get(url).then(res => {
+            if (res.status === 200 && res.data.errmsg === 'success') {
+                resolve({
+                    lat: res.data.data.branch.lat,
+                    lng: res.data.data.branch.lng,
+                    address: res.data.data.branch.address,
+                })
+            } else {
+                reject();
+            }
+        }).catch(err => {
+            reject(err)
+        })
+    })
+}
 async function getList(pageIndex = 1) {
+    console.log(`>>>getList`);
     console.log(pageIndex);
-    let listUrl = `https://open.mishifeng.com/open/activity/list?category_id=0&lat=40.01234890407986&lng=116.5002937825521&page_limit=20&page_num=${pageIndex}`;
+    let listUrl = `https://open.mishifeng.com/open/activity/list?category_id=0&lat=40.01234890407986&lng=116.5002937825521&page_limit=20&page_num=${pageIndex}&zone_id=10`;
     console.log(listUrl);
     let result = await axios.get(listUrl);
 
@@ -32,5 +63,27 @@ async function getList(pageIndex = 1) {
     }
     await sleep(1000)
     await getList(++pageIndex);
+}
+
+async function search(query, region = "北京") {
+    console.log(`>>>search`);
+    let url = `http://api.map.baidu.com/place/v2/search?query=${encodeURIComponent(query)}&region=${encodeURIComponent(region)}&output=json&ak=${ak}`;
+    console.log(url);
+    return new Promise((resolve, reject) => {
+        axios.get(url).then(res => {
+            console.log(res);
+            if (res.status === 200 && res.data.status == 0 && res.data.results && res.data.results.length) {
+                let list = res.data.results;
+                resolve(list[0].location)
+            } else {
+                console.log('fail');
+                reject();
+            }
+        }).catch(err => {
+            console.log(err);
+            reject()
+        })
+    })
+
 }
 main();
